aebb260938c36dda07fc2dc555d5eec2b854e1a4
Jose/Chelu/Xulo <jjesusramon@cifpfbmoll.eu>
2023-03-15 22:28:43 +0100

    feat(Casos tests para lógica)

---
+++ test/domain/individualTests/test_agedBrie.py
@@ -0,0 +1,32 @@
+import pytest
+
+
+@pytest.fixture
+def brie():
+    test_brie = AgedBrie("Aged Brie", 2, 0)
+
+    return test_brie
+
+
+@pytest.mark.agedBrie
+def test_updateQuality(brie):
+
+    test_sell_in = 2
+    test_quality = 0
+
+    for i in range(0, 12):
+        if test_sell_in > 0:
+            test_quality += 1
+
+        elif test_quality < 0:
+            test_quality = 0
+
+        elif test_sell_in <= 0:
+            test_quality += 2
+
+        brie.updateQuality()
+        test_sell_in -= 1
+
+        assert brie.sell_in == test_sell_in
+
+        assert brie.quality == test_quality
